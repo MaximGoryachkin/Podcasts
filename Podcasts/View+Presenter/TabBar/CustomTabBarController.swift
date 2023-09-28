8b10f3815351a7c7e7dbd62da371affdaa8b4c57
Mirasque <obscure1321@gmail.com>
2023-09-28 12:01:15 +0300

    custom tabbar design is ready

--- Podcasts/View+Presenter/TabBar/CustomTabBarController.swift
+++ Podcasts/View+Presenter/TabBar/CustomTabBarController.swift
@@ -1,37 +1,30 @@
 //
-//  CustomTabBarController.swift
+//  CTabBar.swift
 //  Podcasts
 //
-//  Created by Miras Maratov on 27.09.2023.
+//  Created by Miras Maratov on 28.09.2023.
 //
 
 import UIKit
 
-class CustomTabBarController : UITabBarController, UITabBarControllerDelegate {
-    
+class CustomTabBarController: UITabBarController {
+// MARK: - life cycle func
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpView()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        tabBar.backgroundColor = .clear
+        generateTabBar()
+        setTabBarApearence()
     }
 }
 
+// MARK: - extension
 extension CustomTabBarController {
-    private func setUpView () {
-        delegate = self
-        createTabBar()
-    }
-    
-    private func createTabBar() {
+// MARK: - flow funcs
+    private func generateTabBar() {
         viewControllers = [
-        makeVC(vc: ViewController(), image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill")),
-        makeVC(vc: UIViewController(), image:UIImage(systemName: "magnifyingglass.circle") , selectedImage: UIImage(systemName: "magnifyingglass.circle.fill")),
-        makeVC(vc: UIViewController(), image: UIImage(systemName: "bookmark.circle"), selectedImage: UIImage(systemName: "bookmark.circle.fill")),
-        makeVC(vc: UIViewController(), image: UIImage(systemName: "gearshape"), selectedImage: UIImage(systemName: "gearshape.fill"))
+        makeVC(vc: ViewController(), image: UIImage(named: "home/inactive"), selectedImage: UIImage(named: "home/active")),
+        makeVC(vc: UIViewController(), image: UIImage(named: "search/inactive"), selectedImage: UIImage(named: "search/active")),
+        makeVC(vc: UIViewController(), image: UIImage(named: "bookmark/inactive"), selectedImage: UIImage(named: "bookmark/active")),
+        makeVC(vc: UIViewController(), image: UIImage(named: "settings/inactive"), selectedImage: UIImage(named: "settings/active"))
         ]
     }
     
@@ -39,5 +32,22 @@ extension CustomTabBarController {
         vc.tabBarItem.image = image
         vc.tabBarItem.selectedImage = selectedImage
         return vc
+    }
+    
+    private func setTabBarApearence() {
+        let positionX: CGFloat = 24
+        let positionY: CGFloat = 14
+        let width = tabBar.bounds.width - positionX * 2
+        let height = 68.0
+        
+        let roundLayer = CAShapeLayer()
+        
+        let bezierPath = UIBezierPath(roundedRect: CGRect(x: positionX, y: tabBar.bounds.minY - positionY, width: width, height: height), cornerRadius: 20)
+        roundLayer.path = bezierPath.cgPath
+        
+        tabBar.layer.insertSublayer(roundLayer, at: 0)
+        tabBar.itemWidth = width / 6.5
+        tabBar.itemPositioning = .centered
+        roundLayer.fillColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 }
