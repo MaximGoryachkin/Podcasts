bd195733e9dcbf0954e9b4e8061bef8924f77a63
Mirasque <obscure1321@gmail.com>
2023-09-29 18:41:08 +0300

    layout of 2 search screens is probably finished

--- Podcasts/View+Presenter/Search/SearchViewController.swift
+++ Podcasts/View+Presenter/Search/SearchViewController.swift
@@ -12,7 +12,7 @@ class SearchViewController: UIViewController {
     private let searchLabel: UILabel = {
         let element = UILabel()
         element.text = "Search"
-        element.font = UIFont(name: "Manrope-Regular", size: 16)
+        element.font = UIFont(name: "Manrope-Bold", size: 16)
         element.textAlignment = .center
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
@@ -21,7 +21,25 @@ class SearchViewController: UIViewController {
     private let searchView: UIView = {
         let element = UIView()
         element.layer.cornerRadius = 12
-        element.backgroundColor = .lightGray
+        element.backgroundColor = #colorLiteral(red: 0.6557124077, green: 0.9678753018, blue: 0.9384237844, alpha: 1)
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let textField: UITextField = {
+        let element = UITextField()
+        element.placeholder = "Podcast, chanel or artist"
+        element.clearButtonMode = .whileEditing
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let loupe: UIImageView = {
+        let element = UIImageView()
+        element.backgroundColor = .clear
+        element.image = UIImage(named: "search/inactive")
+        element.contentMode = .scaleAspectFit
+        element.clipsToBounds = true
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
@@ -29,28 +47,48 @@ class SearchViewController: UIViewController {
     private let genresLabel: UILabel = {
         let element = UILabel()
         element.text = "Top genres"
-        element.font = UIFont(name: "Manrope-Regular", size: 16)
+        element.font = UIFont(name: "Manrope-Bold", size: 16)
         element.textAlignment = .center
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
     
-    private let seeAllLabel: UILabel = {
+    private let seeAllButton: UIButton = {
+        let element = UIButton()
+        element.backgroundColor = .clear
+        element.setTitle("See all", for: .normal)
+        element.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .thin)
+        element.setTitleColor(.darkGray, for: .normal)
+        element.contentHorizontalAlignment = .right
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let browseLabel: UILabel = {
         let element = UILabel()
-        element.text = "See all"
-        element.textColor = .lightGray
-        element.font = UIFont.systemFont(ofSize: 16, weight: .thin)
+        element.text = "Browse all"
+        element.font = UIFont(name: "Manrope-Bold", size: 16)
         element.textAlignment = .center
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
     
-    private let genresTableView: UITableView = {
-        let element = UITableView()
-        element.backgroundColor = .lightGray
-        element.alwaysBounceHorizontal = true
-        element.alwaysBounceVertical = false
-        element.separatorStyle = .none
+    private let genresCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let element = UICollectionView(frame: .zero, collectionViewLayout: layout) // Указываем начальный frame
+        element.backgroundColor = .clear
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let verticalCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        
+        let element = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        element.backgroundColor = .clear
         element.translatesAutoresizingMaskIntoConstraints = false
         return element
     }()
@@ -58,26 +96,36 @@ class SearchViewController: UIViewController {
 // MARK: - life cycle func
     override func viewDidLoad() {
         super.viewDidLoad()
+        setCollection()
         setUpViews()
     }
     
 // MARK: - flow funcs
+    private func setCollection() {
+        genresCollectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
+        genresCollectionView.delegate = self
+        genresCollectionView.dataSource = self
+        view.addSubview(genresCollectionView)
+        genresCollectionView.showsHorizontalScrollIndicator = false
+        
+        verticalCollectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: "VerticalCollectionViewCell")
+        verticalCollectionView.delegate = self
+        verticalCollectionView.dataSource = self
+        view.addSubview(verticalCollectionView)
+        verticalCollectionView.showsVerticalScrollIndicator = false
+    }
+    
     private func setUpViews() {
         view.backgroundColor = .white
         view.addSubview(searchView)
+        searchView.addSubview(textField)
+        searchView.addSubview(loupe)
         view.addSubview(searchLabel)
         view.addSubview(genresLabel)
-        view.addSubview(seeAllLabel)
-        view.addSubview(genresTableView)
-        setDelegate()
-        genresTableView.transform = CGAffineTransform(rotationAngle: -.pi / 2)
+        view.addSubview(seeAllButton)
+        view.addSubview(browseLabel)
         setConstraints()
-    }
-    
-    private func setDelegate() {
-        genresTableView.delegate = self
-        genresTableView.dataSource = self
-        genresTableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        seeAllButton.addTarget(self, action: #selector(goToResult), for: .touchUpInside)
     }
     
     private func setConstraints() {
@@ -91,39 +139,84 @@ class SearchViewController: UIViewController {
             searchView.topAnchor.constraint(equalTo: searchLabel.bottomAnchor, constant: 34),
             searchView.heightAnchor.constraint(equalToConstant: 48),
             
+            loupe.trailingAnchor.constraint(equalTo: searchView.trailingAnchor, constant: -24),
+            loupe.topAnchor.constraint(equalTo: searchView.topAnchor, constant: 12),
+            loupe.bottomAnchor.constraint(equalTo: searchView.bottomAnchor, constant: -12),
+            loupe.widthAnchor.constraint(equalToConstant: 24),
+            
+            
+            textField.leadingAnchor.constraint(equalTo: searchView.leadingAnchor, constant: 24),
+            textField.topAnchor.constraint(equalTo: searchView.topAnchor, constant: 12),
+            textField.bottomAnchor.constraint(equalTo: searchView.bottomAnchor, constant: -12),
+            textField.trailingAnchor.constraint(equalTo: loupe.leadingAnchor, constant: -26),
+            
             genresLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 198),
             genresLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
             genresLabel.heightAnchor.constraint(equalToConstant: 22),
             
-            seeAllLabel.topAnchor.constraint(equalTo: genresLabel.topAnchor),
-            seeAllLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -28),
+            seeAllButton.topAnchor.constraint(equalTo: genresLabel.topAnchor),
+            seeAllButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -28),
+            seeAllButton.heightAnchor.constraint(equalToConstant: 24),
+            seeAllButton.widthAnchor.constraint(equalToConstant: 49),
             
-            genresTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 231),
-            genresTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
-            genresTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            genresTableView.heightAnchor.constraint(equalToConstant: 84)
+            genresCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 231),
+            genresCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            genresCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            genresCollectionView.heightAnchor.constraint(equalToConstant: 84),
+            
+            browseLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 339),
+            browseLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            browseLabel.heightAnchor.constraint(equalToConstant: 22),
+            
+            verticalCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 382),
+            verticalCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+            verticalCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32),
+            verticalCollectionView.heightAnchor.constraint(equalToConstant: 384)
         ])
+    }
+    
+    @objc private func goToResult(){
+        let vc = ResultViewController()
+        vc.view.alpha = 0.0
+        addChild(vc)
+        vc.view.frame = self.view.bounds
+        self.view.addSubview(vc.view)
+        
+        UIView.animate(withDuration: 0.3) {
+            vc.view.alpha = 1.0
+        }
+        vc.didMove(toParent: self)
     }
 }
 
 // MARK: - extension
-extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        8
+extension SearchViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        24
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 84
-    }
-    
-    func tableView(_ tableView: UITableView, widthForRowAt indexPath: IndexPath) -> CGFloat {
-        return 147
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = genresTableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell else {
-            return UITableViewCell()
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == genresCollectionView {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+            cell.configure()
+            return cell
+        } else if collectionView == verticalCollectionView {
+            // Иной идентификатор и настройку для вертикальной коллекции
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "VerticalCollectionViewCell", for: indexPath) as? CollectionViewCell else { return UICollectionViewCell() }
+            cell.configure()
+            return cell
+        } else {
+            return UICollectionViewCell()
         }
-        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let cellWidth = Int((verticalCollectionView.frame.width - 17) / 2) // 17 - минимальный интервал между ячейками
+            let cellHeight = 84
+            return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 17.0 //отступы между ячейками
     }
 }
--- Podcasts/View+Presenter/Search/TableViewCell.swift
+++ Podcasts/View+Presenter/Search/TableViewCell.swift
@@ -2,15 +2,50 @@
 //  TableViewCell.swift
 //  Podcasts
 //
-//  Created by Miras Maratov on 28.09.2023.
+//  Created by Miras Maratov on 29.09.2023.
 //
 
 import UIKit
 
 class TableViewCell: UITableViewCell {
+    private let mainView: UIView = {
+        let element = UIView()
+        element.backgroundColor = UIColor(red: 0.929, green: 0.941, blue: 0.988, alpha: 1)
+        element.layer.cornerRadius = 16
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let avatarView: UIView = {
+        let element = UIView()
+        element.backgroundColor = .customLightBlue
+        element.layer.cornerRadius = 16
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let podcastName: UILabel = {
+        let element = UILabel()
+        element.text = "The tale of greatest warrior"
+        element.font = UIFont(name: "Manrope-Regular", size: 14)
+        element.textAlignment = .left
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private let shortInfo: UILabel = {
+        let element = UILabel()
+        element.text = "56:38 • 82 Eps"
+        element.font = UIFont.systemFont(ofSize: 12, weight: .thin)
+        element.textAlignment = .left
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: "TableViewCell")
-        backgroundColor = .cyan
+        setUpCell()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -19,5 +54,35 @@ class TableViewCell: UITableViewCell {
     
     override func prepareForReuse() {
         super.prepareForReuse()
+    }
+    
+    func setUpCell() {
+        addViews()
+        NSLayoutConstraint.activate([
+            mainView.topAnchor.constraint(equalTo: topAnchor),
+            mainView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
+            mainView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            mainView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            avatarView.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 8),
+            avatarView.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 8),
+            avatarView.heightAnchor.constraint(equalToConstant: 56),
+            avatarView.widthAnchor.constraint(equalToConstant: 56),
+            
+            podcastName.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 16),
+            podcastName.leadingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: 20),
+            podcastName.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -5),
+            
+            shortInfo.bottomAnchor.constraint(equalTo: mainView.bottomAnchor, constant: -16),
+            shortInfo.leadingAnchor.constraint(equalTo: avatarView.trailingAnchor, constant: 20),
+            shortInfo.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -5),
+        ])
+    }
+    
+    func addViews() {
+        addSubview(mainView)
+        mainView.addSubview(avatarView)
+        mainView.addSubview(podcastName)
+        mainView.addSubview(shortInfo)
     }
 }
