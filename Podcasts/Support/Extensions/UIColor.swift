a49682f8d48c18de61b52c25627358032a3283a9
Arthur <Fokusnik90999@gmail.com>
2023-09-29 17:22:29 +0500

    initial commit

--- Podcasts/Support/Extensions/UIColor.swift
+++ Podcasts/Support/Extensions/UIColor.swift
@@ -10,4 +10,5 @@ import UIKit.UIColor
 extension UIColor {
     static let customBlue = UIColor(red: 40/255, green: 130/255, blue: 241/255, alpha: 1)
     static let customLightBlue = UIColor(red: 40/255, green: 130/255, blue: 241/255, alpha: 0.37)
+    static let customGray = UIColor(red: 232/255, green: 236/255, blue: 251/255, alpha: 1)
 }
