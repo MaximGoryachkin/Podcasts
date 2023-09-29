b50347ce358ac3f22730ee2642300997a59a2663
Arthur <Fokusnik90999@gmail.com>
2023-09-29 17:23:30 +0500

    returned root controller

--- Podcasts/Application/SceneDelegate.swift
+++ Podcasts/Application/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = ProfileSettingPresenter()
+        window.rootViewController = ViewController()
         window.makeKeyAndVisible()
         self.window = window
     }
